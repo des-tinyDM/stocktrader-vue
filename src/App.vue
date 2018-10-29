5049a835a20ef9a89f49a0bcf522600bfa9ad52f
des-tinyDM <destinyleaross@gmail.com>
2018-10-28 19:11:59 -0500

    initial commit, before functional portfolio vuex model was created

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <st-header></st-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    "st-header": Header
+  },
+  created() {
+    this.$store.dispatch("initStocks");
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  padding: 30px;
+}
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Stocks View</h1>
+    <st-stock v-bind:key="index" v-for="(stock, index) in stocks" :stock="stock"></st-stock>
+  </div>
+</template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+  components: {
+    "st-stock": Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
+    }
+  }
+};
+</script>
